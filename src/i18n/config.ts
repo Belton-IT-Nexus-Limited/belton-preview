6d61ae613386bc1775b67c13c410afb8b26eea3c
Connor Berghoffer <connorberghoffer@Connors-MBP.local>
2026-01-09 09:19:10 +1300

    feat: create i18n configuration and translation files for en-NZ and en-AU

---
+++ src/i18n/config.ts
@@ -0,0 +1,42 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import enNZCommon from './locales/en-NZ/common.json'
+import enNZNavigation from './locales/en-NZ/navigation.json'
+import enNZHome from './locales/en-NZ/pages/home.json'
+import enAUCommon from './locales/en-AU/common.json'
+import enAUNavigation from './locales/en-AU/navigation.json'
+import enAUHome from './locales/en-AU/pages/home.json'
+
+const resources = {
+  'en-NZ': {
+    common: enNZCommon,
+    navigation: enNZNavigation,
+    'pages/home': enNZHome
+  },
+  'en-AU': {
+    common: enAUCommon,
+    navigation: enAUNavigation,
+    'pages/home': enAUHome
+  }
+}
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en-NZ',
+    defaultNS: 'common',
+    ns: ['common', 'navigation', 'pages/home'],
+    interpolation: {
+      escapeValue: false
+    },
+    detection: {
+      order: ['path', 'localStorage', 'navigator'],
+      lookupLocalStorage: 'belton-i18n-lng',
+      caches: ['localStorage']
+    }
+  })
+
+export default i18n
